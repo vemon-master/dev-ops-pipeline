f0522603d890a011c5e2c05a0ce4e0377f01c2fc
vemon-master <linuscode.linus@gmail.com>
2026-02-22 18:26:12 +0300

    Feat: CI/CD pipelines pro

--- test.js
+++ test.js
@@ -1,3 +1,6 @@
 console.log('Starting tesst...');
-timeout(() => console.log('Waiting 3 seconds'), 3000);
+setTimeout(() => {
+    console.log("Waiting 3 seconds");
+    process.exit(0);
+}, 3000);
 console.log('Test completed');
